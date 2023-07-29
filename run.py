d44cb6b7f72245496f18eb79e025f0301577fc8b
Siddharth Singh Tanwar <siddharthtanwar28@gmail.com>
2023-07-29 14:36:18 +0530

    Add files via upload

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
-from NoDues import app
+# from nodues import create_app, db
+# app = create_app()
+from nodues import app, db
+
+# If you need to create the database tables, you can do it here
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
+
