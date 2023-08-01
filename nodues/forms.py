a94d51fd7d7ef30dd05832557901527555bff16f
SiddharthSinghTanwar <siddharthtanwar28@gmail.com>
2023-08-01 12:46:21 +0530

    added login required for home page & profile photo

--- nodues/forms.py
+++ nodues/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed, FileField
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -43,6 +44,7 @@ class UpdateAccountForm(FlaskForm):
                            validators=[DataRequired(), Length(min=2, max=20)])
     enrollment_no = StringField('Enrollment No',
                         validators=[DataRequired()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
     course = StringField('Course',
