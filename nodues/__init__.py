90e5609c60df6a4b1179ed4f9f80794448c8f69a
Siddharth Singh Tanwar <siddharthtanwar28@gmail.com>
2023-08-26 20:09:13 +0530

    Update __init__.py

--- nodues/__init__.py
+++ nodues/__init__.py
@@ -7,28 +7,30 @@ from flask_mail import Mail
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
-mail = Mail()
+oad_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ea77a2c1cff84971c247e008b1a749fd'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'yourEmail@mail.com'
+app.config['MAIL_PASSWORD'] = 'your_password'
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_DEFAULT_SENDER'] = 'nodues06@gmail.com'
+
+mail = Mail()
 
 # Get the environment variables
-MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+# MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+# MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+
     
 mail.init_app(app)
 
